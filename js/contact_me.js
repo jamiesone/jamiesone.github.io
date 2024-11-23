68e428187bd9217ddbb01778e103e6cfe35233a9
Emily <jamiesone@outlook.com>
2024-11-23 11:00:07 +0100

    video

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,8 +39,8 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        // .append("<strong>We're looking forward to celebrating with you! </strong>");
-                        .append("<strong>On se réjouit de fêter notre mariage avec vous! </strong>");
+                        .append("<strong>We're looking forward to celebrating with you! </strong>");
+                        // .append("<strong>On se réjouit de fêter notre mariage avec vous! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
